eae8c7a45f7d92737406a49320e2d254150f3f03
devadharshini <devadharshinit.22aid@kongu.edu>
2026-03-12 20:24:29 +0530

    Initial Login Page

---
+++ Login/src/main/java/LoginServlet.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        if(username.equals("admin") && password.equals("1234")) {
+            response.sendRedirect("success.jsp");
+        } else {
+            response.getWriter().println("Invalid Login");
+        }
+    }
+}
---
+++ Login/src/main/webapp/login.jsp
@@ -0,0 +1,24 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<div align = center>
+<h1>User Login</h1></div>
+<form action="login" method="post">
+
+Username:
+<input type="text" name="username"><br>
+
+Password:
+<input type="password" name="password"><br>
+
+<input type="submit" value="Login">
+
+</form>
+</body>
+</html>
